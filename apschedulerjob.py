b92ad694b28eeaf1ca211c35fa49d57c670911db
liuyu <823893795@qq.com>
2019-09-23 11:58:25 +0800

    检测如果已经爬取过的信息则不再进行爬取

--- apschedulerjob.py
+++ apschedulerjob.py
@@ -5,6 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+visited_fd = set()
+
 
 def spider_common(url, Model, page, name):
     print("开始爬取：" + name)
@@ -18,39 +20,40 @@ def spider_common(url, Model, page, name):
         soup = BeautifulSoup(r1.text, 'html.parser')
         item_list = soup.find(name='div', attrs={'class': 'info'}).find('ul').find_all('li')
         for item in item_list:
-            time = item.find(name='div', attrs={'class': 'time curr'})
-            day = time.find('span').text
-            year_month = list(time.children)[-1].strip()
-            date_string = year_month + "-" + day
-            date = datetime.strptime(date_string, "%Y-%m-%d")
-
             a = item.find('a')
             href = a['href'] if 'http' in a['href'] else "http://www.ccgp-sichuan.gov.cn"+a['href']
-            title = a.find(name='div', attrs={'class': 'title'}).text
+            if href not in visited_fd:
+                visited_fd.add(href)
+                title = a.find(name='div', attrs={'class': 'title'}).text
+                time = item.find(name='div', attrs={'class': 'time curr'})
+                day = time.find('span').text
+                year_month = list(time.children)[-1].strip()
+                date_string = year_month + "-" + day
+                date = datetime.strptime(date_string, "%Y-%m-%d")
 
-            pur_obj = Model.query.filter(Model.title == title).first()
-            if not pur_obj:
-                if '软件' in title:
-                    print('该公告标题和软件有关系:' + title)
-                    purchase_info = Model()
-                    purchase_info.purchase_time = date
-                    purchase_info.title = title
-                    purchase_info.url = href
-                    db.session.add(purchase_info)
-                    db.session.commit()
-                    continue
-                r2 = requests.get(url=href)
-                r2.encoding = 'utf-8'
-                soup = BeautifulSoup(r2.text, 'html.parser')
-                content = soup.find(name='div', id='myPrintArea').text
-                if '软件' in content:
-                    print('该公告内容和软件有关系:'+title)
-                    purchase_info = Model()
-                    purchase_info.purchase_time = date
-                    purchase_info.title = title
-                    purchase_info.url = href
-                    db.session.add(purchase_info)
-                    db.session.commit()
+                pur_obj = Model.query.filter(Model.title == title).first()
+                if not pur_obj:
+                    if '软件' in title:
+                        print('该公告标题和软件有关系:' + title)
+                        purchase_info = Model()
+                        purchase_info.purchase_time = date
+                        purchase_info.title = title
+                        purchase_info.url = href
+                        db.session.add(purchase_info)
+                        db.session.commit()
+                        continue
+                    r2 = requests.get(url=href)
+                    r2.encoding = 'utf-8'
+                    soup = BeautifulSoup(r2.text, 'html.parser')
+                    content = soup.find(name='div', id='myPrintArea').text
+                    if '软件' in content:
+                        print('该公告内容和软件有关系:'+title)
+                        purchase_info = Model()
+                        purchase_info.purchase_time = date
+                        purchase_info.title = title
+                        purchase_info.url = href
+                        db.session.add(purchase_info)
+                        db.session.commit()
     print("本次爬取结束："+name)
 
 
